adebad2890208bb130d0c1ce125ee256a47a7e0f
Leonardo Chermaut <chermauts@hotmail.com>
2025-04-21 20:51:44 -0300

    refactor: change home page router

--- src/router/router/router.tsx
+++ src/router/router/router.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import { homeRouter } from "@/modules/home/homeRouter.tsx";
-import { HomePage } from "@/modules/home/pages/HomePage.tsx";
 import { notFoundRouter } from "@/modules/notfound/notFoundRouter.tsx";
 import { NotFoundPage } from "@/modules/notfound/pages/NotFoundPage.tsx";
 import { routes } from "./routes.ts";
@@ -10,10 +9,6 @@ export const router = createBrowserRouter([
   {
     path: routes.home,
     children: [...homeRouter, ...notFoundRouter],
-  },
-  {
-    path: "/",
-    element: <HomePage />,
   },
   {
     path: "*",
